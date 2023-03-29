11d8ffd7f223633a4ff1768e953549b35933c83f
Kodiererin <iamujjwal45@gmail.com>
2023-03-29 20:07:03 +0530

    Adding Files

--- testApp.js
+++ testApp.js
@@ -1,14 +1,5 @@
-// https://www.geeksforgeeks.org/javascript-function-complete-reference/?ref=shm
-// Refer this Website and do the changes
-
-
 "use strict"
-
-// Error Coming! During File Reading!
-
 var multer = require('multer');
-const fs = require('fs')
-const path = require('path')
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -25,30 +16,18 @@ var fileupload = require("express-fileupload");
 app.use(fileupload());
 
 // const fs = require('fs');
-
+const path = require('path');
 app.use(bodyParser.json());
-//////////////////////////////////////////////////////////////////////////////////////
-// Setting Up Multer for Storing Image
-
-// var multer = require('multer');
-
-
 var storage = multer.diskStorage({
   destination : (req,file,cb) =>{
     cb(null , '/uploads');        // changed now uploads -> /uploads.
   },
   filename : (req,file,cb) =>{
     // cb(null,file.fieldname + '-' + Date.now()+'-'+file.originalname.split('.').pop());
-    cb(null,Date.now()+file.originalname)
+    cb(null, file.fieldname + '-' + Date.now())
   }
 });
 var upload = multer({ storage: storage,limits: { fileSize: 10 * 1024 * 1024 } });
-// upload.single('image')
-
-// var fileupload = require("express-fileupload");
-// app.use(fileupload());
-/////////////////////////////////////////////////////////////////////////////////////
-
 require('dotenv/config')
 
 async function main() {
@@ -61,18 +40,6 @@ async function main() {
 mongoose.set('strictQuery',true);
 
 const blogModel = require('./models/Database')
-
-
-// Creating a default data for testing 
-// const myTestBlog = new blogModel({
-//   blogTitle : "Hello World I am Ujjwal Kumar",
-//   blogBody : "I am Ujjwal Kumar, I am from India and I am doing Engineering",
-// });
-
-// myTestBlog.save();
-
-
-// -----------------------------------------------------------------------------------------------------------------------------------
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -113,56 +80,14 @@ app.get("/",async function(req,res)
     }
     });
 
-
-    // posts.forEach(function(elements)
-    // {
-    //   // console.log(_.lowerCase(elements.title));      // check krne ke Liye
-    //   // console.log(_.lowerCase(req.params.test));
-    //   let ctr=0;
-    //   if(_.lowerCase(elements.title) == _.lowerCase(req.params.test))
-    //   {
-    //     res.render("post",{title : elements.title , body : elements.content,title : elements.title});
-    //   }
-    // })
-
  })
 
  app.post("/compose" , upload.single('image') , function(req,res)
  {
-  console.log(req.body.postTitle);
-    try{
-      // posts.push(post);
-      myBlog = new blogModel({
-        blogTitle : req.body.postTitle,
-        blogBody : req.body.postBody,
-        img: {
-          // data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.originalname.split('.').pop())),
-          data: fs.readFileSync(req.file.path),
-          contentType: 'image/png'
-        }
-      })
-    console.log(myBlog);
-    }catch(e){
-      console.log(e);
-    }
-//   // Saving the Post
-//   await myBlog.save(function(err,data){
-//     if(!err && data){
-//       console.log("Data Saved");
-//     }
-//     else{
-//       console.log(err);
-//     }
-//   });
-
-//   res.redirect("/");
-
-//   // console.log(data.title);
+  console.log(req.body);
+  console.log(req.file.filename);
  })
 
-
-
-// Testing a file
 app.get('/testCompose',function(req,res){
   res.render('testCompose');
 })
